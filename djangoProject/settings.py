3284de5d8a7a80564d029d49fdee99e78ea07c50
rashed01 <irashdad42@gmail.com>
2021-10-16 08:35:06 +0600

    df

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,4 +128,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT=  "/var/www/example.com/static/"
+STATIC_ROOT = BASE_DIR/'assets'
+
+django_heroku.settings(locals())
